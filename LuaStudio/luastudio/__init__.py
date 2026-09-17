7fee3af389c92b6572dfe951fe614f2ac94ea044
NandoX LUA 🌙 <nand00xustar1@gmail.com>
2026-09-17 01:39:46 -0300

    Delete LuaStudio directory

--- LuaStudio/luastudio/__init__.py
+++
@@ -1,3 +0,0 @@
-from .runtime import Runtime
-from .api import Scene, Instance, CreateRoot
-__all__ = ["Runtime", "Scene", "Instance", "CreateRoot"]
--- LuaStudio/luastudio/lua/__init__.py
+++
@@ -1,7 +0,0 @@
-from .interp import Interpreter, LuaTable, LuaError, LuaFunction, tostring, tonumber, truthy, lua_type, from_py, to_py, as_list, first
-from .parser import parse
-from .lexer import LuaSyntaxError
-
-__all__ = ["Interpreter", "LuaTable", "LuaError", "LuaFunction", "LuaSyntaxError",
-           "tostring", "tonumber", "truthy", "lua_type", "from_py", "to_py",
-           "as_list", "first", "parse"]
